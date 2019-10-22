10d8ddb9599b7b31c63fbfac760571f03c117130
alde103 <aldebaran_103@hotmail.com>
2019-10-21 20:02:29 -0500

    libplctag source code added

---
+++ lib/tag.ex
@@ -0,0 +1,18 @@
+defmodule Abex.Tag do
+  @moduledoc """
+  Documentation for Abex.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Abex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
--- mix.exs
+++ mix.exs
@@ -2,11 +2,14 @@ defmodule Abex.MixProject do
   use Mix.Project
 
   def project do
+    #elixir cmake should put compile as default path.
+    System.put_env("MIX_COMPILE_PATH", Mix.Project.compile_path(build_per_environment: true, app: :abex))
     [
       app: :abex,
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
+      compilers: [:cmake] ++ Mix.compilers(),
       deps: deps()
     ]
   end
@@ -21,6 +24,7 @@ defmodule Abex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:elixir_cmake, "~> 0.1.0"},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
